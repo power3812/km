cbd4d5c8546059a5b2b1863e3c9a3a7c02d76852
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-09-21 08:02:25 +0900

    初期データを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Color.create!(name: "black", image: "dark", r: 0, g: 0, b: 0)
+require 'csv'
+csv_data = CSV.read('db/colorimages.csv', headers: true)
+csv_data.each do |data|
+  Color.create!(name: data[1], image: data[2], r: data[3], g: data[4], b: data[5])
+end
 History.create!(r: 0, g: 0, b: 0)
