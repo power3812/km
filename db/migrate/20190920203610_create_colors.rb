b6dfa53a86925ce0bca0f5dfe5793701d4df874c
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-09-21 06:58:20 +0900

    colorテーブルのマイグレーションファイルを追加

---
+++ db/migrate/20190920203610_create_colors.rb
@@ -0,0 +1,13 @@
+class CreateColors < ActiveRecord::Migration[5.1]
+  def change
+    create_table :colors do |t|
+      t.text :name
+      t.text :image
+      t.integer :r
+      t.integer :g
+      t.integer :b
+
+      t.timestamps
+    end
+  end
+end
