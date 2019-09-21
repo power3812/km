44a5d9bfb44579c32dfcc741cecfd872c2240b40
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-09-21 11:00:59 +0900

    shopテーブルの作成

---
+++ db/migrate/20190921011625_create_clothes.rb
@@ -0,0 +1,13 @@
+class CreateClothes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :clothes do |t|
+      t.text :name
+      t.text :jan
+      t.integer :r
+      t.integer :g
+      t.integer :b
+
+      t.timestamps
+    end
+  end
+end
