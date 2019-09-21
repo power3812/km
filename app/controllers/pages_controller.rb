9cb77077f692d08788e81a9cb5e9d7f7d39d67bd
opuoth <keisukewithaaa@gmail.com>
2019-09-21 10:51:06 +0900

    rgb探索

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,19 @@ class PagesController < ApplicationController
   def index
     @history=History.last
     @color=nil
+    @colors=Color.all
     if @history
+      min=1000000000
+      @colors.each do|t|
+        cons=(t.r-@history.r)**2+(t.g-@history.g)**2+(t.b-@history.b)**2
+        if min>=cons
+          min=cons
+          @r=t.r
+          @g=t.g
+          @b=t.b
+        end
+      end
       #定義に基づいた条件式
-      @r=@history.r
-      @g=@history.g
-      @b=@history.b
       @color=Color.find_by(r:@r,g:@g,b:@b)
       respond_to do |format|
         format.html
